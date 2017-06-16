3b446ae69b8707dbd3e8cdfd472b4264327b72b7
Aman Deep <aman2speak@gmail.com>
2017-06-17 02:57:39 +0530

    Initial commit

--- js/new.js
+++ js/new.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         app.cards[random] = temp;//Randomizing the array i.e reshuffling the cards.
       }
       app.assignCards();
-      //console.log('Shuffled Card Array: ' + app.cards);
+      console.log('Shuffled Card Array: ' + app.cards);
     },
     assignCards: function() {
       $('.card').each(function(index) {
@@ -35,33 +35,38 @@ $(document).ready(function() {
       });
     },
     check: function() {
-      if ($('.selected').length === 2) {
-        if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {
+      if ($('.selected').length === 2) {//3 equal to sign is added knowingly search google to know more.
+        if ($('.selected').first().data('cardValue') == $('.selected').last().data('cardValue')) {//comparing two values so first and last used.
           $('.selected').each(function() {
             $(this).animate({
               opacity: 0
             }).removeClass('unmatched');
-           
+          
             score+=20;
+            match++;
           });
           $('.selected').each(function() {
             $(this).removeClass('selected');
+            score=score-10;
             chance++;
-            score+= -10;
           });
           app.checkWin();
+          
+           
+            $("#match").html("<br>Matches <br>"+match/2);
+          $("#score").html("<br>Score <br>"+score);
         } else {
           setTimeout(function() {
             $('.selected').each(function() {
               $(this).html('').removeClass('selected');
             });
-          }, 750);
+          }, 700);
         }
       }
     },
     checkWin: function() {
       if ($('.unmatched').length === 0) {
-        alert('You Won!! ' +'Your score is: '+score +' You took a total of '+chance+' chances.');
+        alert('You Won!! ' +'Your score is: '+score );
       }
     }
   };
@@ -69,6 +74,8 @@ $(document).ready(function() {
 $("footer").hide();
 $("#rules").hide();
 $(".status").hide();
+$(".head").hide();
+$(".clock").hide();
 $("#play").click(function(){
   $("#play").hide(750);
   app.play();
@@ -78,14 +85,32 @@ $("#play").click(function(){
   $(".rules").hide(1000);
   $(".status").show(1000);
   $(".head").css('height', '28%');
+  $(".clock").show(2000);
 });
 $("#rules").click(function(){
   $(".container").hide();
   $("footer").hide();
     $("#rules").hide();
     $(".rules").show(2000);
+    $(".clock").hide();
     $("#play").show(2000);
+    $(".head").css({
+      'height': '48%',
+      'color': 'wheat'
+    });
 
 });
+$("#start").click(function(){
+  $(".head").show(1000);
+  $("#start").hide(750);
+  $("#load").hide(2000);
+
+});
+$("#End").click(function() {
+  /* Act on the event */
+  alert('Your score is: '+score );
+  app.play();
+});
+
   //app.play();
 });
